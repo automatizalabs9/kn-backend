2297900b1cace2e83dd810e9aaad53c83c6799c0
automatizalabs9 <automatizalabs9@gmail.com>
2025-12-05 19:17:29 -0300

    Update leads.ts

--- api/leads.ts
+++ api/leads.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { getPool, buildQueryContext } from "../_lib/db";
+import { getPool, buildQueryContext } from "./_lib/db";
 
 function setCorsHeaders(res: VercelResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -13,7 +13,7 @@ function setCorsHeaders(res: VercelResponse) {
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   let pool;
-  
+
   try {
     setCorsHeaders(res);
 
